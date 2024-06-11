ceb0407829730155325160dcc8daee6b12e4830f
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2024-06-10 22:26:47 -0300

    criando repository de person e services

--- src/person/dto/update-person.dto.ts
+++ src/person/dto/update-person.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePersonDto } from './create-person.dto';
 
-export class UpdatePersonDto extends PartialType(CreatePersonDto) {}
+export class UpdatePersonDto extends PartialType(CreatePersonDto) {
+  public email: string;
+  public name: string;
+  public bornDate: Date;
+  public cpf: string;
+}
--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { Repository } from 'typeorm';
+import { Person } from './entities/person.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PersonService {
-  create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+  private readonly personRepository: Repository<Person>;
+  constructor(@InjectRepository(Person) personRepository: Repository<Person>) {
+    this.personRepository = personRepository;
   }
 
-  findAll() {
-    return `This action returns all person`;
+  public async create(createPersonDto: CreatePersonDto): Promise<Person> {
+    return await this.personRepository.create(createPersonDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} person`;
+  public async findAll(): Promise<Person[]> {
+    return await this.personRepository.find();
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  public async findOne(id: number): Promise<Person> {
+    return await this.personRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  public async update(
+    id: number,
+    updatePersonDto: UpdatePersonDto,
+  ): Promise<Person> {
+    return await this.personRepository.save({
+      id: id,
+      bornDate: updatePersonDto.bornDate,
+      cpf: updatePersonDto.cpf,
+      name: updatePersonDto.name,
+    });
+  }
+
+  public async remove(id: number) {
+    await this.personRepository.delete({ id: id });
   }
 }
