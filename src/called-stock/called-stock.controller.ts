f57e3418ec424172467e8db0eab921a8f8f50fbb
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2024-06-22 18:15:09 -0300

    Alterando modo de criação de auth

--- src/called-stock/called-stock.controller.ts
+++ src/called-stock/called-stock.controller.ts
@@ -1,14 +1,13 @@
-import {Body, Controller, Delete, Get, Param, Post, Query} from '@nestjs/common';
-import {CalledStock} from "./entities/called-stock.entity";
-import {CalledStockService} from "./called-stock.service";
-import {CreateCalledStockDto} from "./dto/create-called-stock.dto";
-import {toCalledStock} from "./converter/create-called-stock.converter";
-import {ViewCalledStockDto} from "./dto/view-called-stock.dto";
-import {toViewCalledStockDto} from "./converter/view-called-stock.converter";
+import { Body, Controller, Delete, Get, Param, Post, Query, } from '@nestjs/common';
+import { CalledStockService } from './called-stock.service';
+import { CreateCalledStockDto } from './dto/create-called-stock.dto';
+import { toCalledStock } from './converter/create-called-stock.converter';
+import { ViewCalledStockDto } from './dto/view-called-stock.dto';
+import { toViewCalledStockDto } from './converter/view-called-stock.converter';
 
 @Controller('calledstocks')
 export class CalledStockController {
-  constructor(private readonly calledStockService: CalledStockService) {}
+  constructor(private readonly calledStockService: CalledStockService) { }
 
   @Get()
   async findAll(): Promise<ViewCalledStockDto[]> {
@@ -16,13 +15,13 @@ export class CalledStockController {
   }
 
   @Get('/between/date')
-  async findBetweenDates(@Query("startDate") startDate: Date, @Query("endDate") endDate: Date): Promise<ViewCalledStockDto[]> {
+  async findBetweenDates(@Query('startDate') startDate: Date, @Query('endDate') endDate: Date): Promise<ViewCalledStockDto[]> {
     return (await this.calledStockService.findBetweenDates(startDate, endDate)).map(c => toViewCalledStockDto(c));
   }
 
   @Post(':displaySymbol')
   async create(@Param('displaySymbol') symbol: string,
-               @Body() calledStock: CreateCalledStockDto): Promise<ViewCalledStockDto> {
+    @Body() calledStock: CreateCalledStockDto): Promise<ViewCalledStockDto> {
     const walletId: number = 1;
     return toViewCalledStockDto(await this.calledStockService.create(toCalledStock(calledStock), walletId, symbol));
   }
--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,10 +1,10 @@
-import {Controller, Get, Param} from '@nestjs/common';
-import {WalletService} from './wallet.service';
-import {Wallet} from './entities/wallet.entity';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { WalletService } from './wallet.service';
+import { Wallet } from './entities/wallet.entity';
 
 @Controller('wallets')
 export class WalletController {
-  constructor(private readonly walletService: WalletService) {}
+  constructor(private readonly walletService: WalletService) { }
 
   @Get()
   findAll(): Promise<Wallet[]> {
@@ -14,5 +14,15 @@ export class WalletController {
   @Get(':id')
   findOne(@Param('id') id: number): Promise<Wallet> {
     return this.walletService.findOne(id);
+  }
+
+  @Post(':id')
+  create(@Param('id') id: number): Promise<Wallet> {
+    return this.walletService.create(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.walletService.remove(Number(id));
   }
 }
